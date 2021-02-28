25a90bfa1686e0c402030dc2e8255b42d6304507
Felipe Gutierrez <felipeg@ibm.com>
2021-02-28 09:00:25 -0500

    updated

--- segment-08-spring-boot-security/contacts-app/src/main/java/com/apress/ContactSecurityConfig.java
+++ segment-08-spring-boot-security/contacts-app/src/main/java/com/apress/ContactSecurityConfig.java
@@ -1,12 +1,13 @@
 package com.apress;
 
+
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 
 @Configuration
 public class ContactSecurityConfig extends WebSecurityConfigurerAdapter {
